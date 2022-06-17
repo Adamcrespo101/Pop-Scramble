7e7389e2db68dec239ac0be0cf2d8341a27474bf
Adam Crespo <adam.crespo101@outlook.com>
2022-06-17 12:53:56 -0400

    created the game board layout, ready to start building out backend

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -7,10 +7,10 @@ function Login (){
         <div className="login">
            <form className="login-form">
                 <label for="username" id="username">Username: </label>
-                <input type="text" />
+                <input type="text" style={{background: 'rgba(250, 250, 250, 0.171)'}}/>
                 <br></br>
                 <label for="password" id="password" >Password: </label>
-                <input type="password" style={{marginTop: "5%"}}/>
+                <input type="password" style={{marginTop: "5%", background: 'rgba(250, 250, 250, 0.171)'}}/>
                 <br></br>
                 <button type="submit" style={{border: 'none', background: "rgba(250, 250, 250, 0.171)", marginLeft: "33%",marginTop: "5%", marginBottom: "5%"}}>Login</button>
                 <br></br>
