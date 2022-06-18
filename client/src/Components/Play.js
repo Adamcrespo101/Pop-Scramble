f727e55c2fdfd5ceee2264742b8840b14e8e1bad
Adam Crespo <adam.crespo101@outlook.com>
2022-06-18 10:32:53 -0400

    can grab user input through UI keyboard

--- client/src/Components/Play.js
+++ client/src/Components/Play.js
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 
 
+function Play({words, setWords}){
 
-function Play(){
-
+    const [input, setInput] = useState([])
 
     const keyBoardTop = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
     const keyBoardMiddle = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
@@ -11,8 +12,10 @@ function Play(){
     function onPressBackspace() {
       //  myTextarea.value = myTextarea.value.substring(0, myTextarea.value.length - 1);
     }
-
-
+    function addUserInput(e){
+        setInput(input + e.target.id)
+    }
+    console.log(input)
     return(
         <div className="play">
             <h2 style={{marginLeft: "10%"}}>Welcome, User!</h2>
@@ -21,6 +24,11 @@ function Play(){
                     <h3 style={{marginLeft: "5%"}}>Lifetime Score:</h3>
                     <h3 style={{marginLeft: "5%"}}>Current Score:</h3>
                 </div>
+                <form>
+                <input type="text" value={input}/>
+                <br></br>
+                <button type="submit" style={{border: 'none', background: "rgba(250, 250, 250, 0.171)", height: "50px", width: "200px"}}>Check your Answer!</button>
+                </form>
                 <div className="word">
                     
                         <h1 className="word-keys">A</h1>
@@ -37,24 +45,24 @@ function Play(){
                     <h1 className="user-input-keys">A</h1>
                 </div>
                 <div className="keyboard">
-                    <div className="top">
+                    <div className="top" >
                     {keyBoardTop.map((key) => {
                         return(
-                            <h2 className="key" value={key}>{key}</h2>
+                            <h2 className="key" onClick={addUserInput} value={key} id={key}>{key}</h2>
                             )
                         })}
                     </div>
                     <div className="mid">
                     {keyBoardMiddle.map((key) => {
                         return(
-                            <h2 className="key" value={key}>{key}</h2>
+                            <h2 className="key" onClick={addUserInput} value={key} id={key}>{key}</h2>
                             )
                         })}
                     </div>
                     <div className="bottom">
                     {keyBoardBottom.map((key) => {
                         return(
-                            <h2 className="key" value={key} >{key}</h2>
+                            <h2 className="key" onClick={addUserInput} value={key} id={key}>{key}</h2>
                             )
                         })}
                     </div>
